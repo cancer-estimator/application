d547fa3dbe720da7dbfda1e8a9076f0c0ee32565
Manoel Vilela <manoel.neto@neoway.com.br>
2024-06-16 01:45:51 -0300

    Update symptons

--- cancer_estimator_application/models.py
+++ cancer_estimator_application/models.py
@@ -1,32 +1,42 @@
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 from pydantic import BaseModel, validator
 
 
 class Symptons(BaseModel):
-    allergy: bool = False
-    fatigue: bool = False
-    chronic_disease: bool = False
+    smoking: bool = False
     coughing: bool = False
-    alcohol_consuming: bool = False
+    alcohol_use: bool = False
     swallowing_difficulty: bool = False
-    peer_pressure: bool = False
+    snoring: bool = False
     anxiety: bool = False
+    fatigue: bool = False
     wheezing: bool = False
     yellow_fingers: bool = False
-    smoking: bool = False
     shortness_of_breath: bool = False
+    allergy: bool = False
     chest_pain: bool = False
+    chronic_lung_disease: bool = False
+    coughing_of_blood: bool = False
+    frequent_cold: bool = False
+    genetic_risk: bool = False
+    obesity: bool = False
+    occupational_hazards: bool = False
+    passive_smoker: bool = False
+    peer_pressure: bool = False
+    dust_allergy: bool = False
+    dry_cough: bool = False
 
 
 # FIXME(@lerax): seg 06 mai 2024 00:56:25
 # inheritance here is just hacky and non-sense, fix this
 class Patient(Symptons):
     name: str
-    sex: str
+    sex: str  # Male / Female
     age: int
     room: str
     hospitalized: bool
     cancer_risk: bool = False
+    cancer_risk_value: Optional[float] = None
     # read only
     symptons: Dict[str, bool] = dict()
 
